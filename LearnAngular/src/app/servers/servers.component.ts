2038880a4d5e6b3529aae51cd91ef0e54f9eb6ee
= <stephensmith@eastman.com>
2018-04-25 20:29:57 -0400

    Branching off for assignment 1

---
+++ LearnAngular/src/app/servers/servers.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+  // styles: [`h3 { color: dodgerblue;} `]
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
